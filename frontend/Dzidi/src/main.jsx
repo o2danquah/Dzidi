f2e9484e2990a8438c00b6d99087f46e7a5058cd
o2danquah <papa10058@gmail.com>
2024-05-10 04:10:52 +0000

    changes to react-router-dom

--- frontend/Dzidi/src/main.jsx
+++ frontend/Dzidi/src/main.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Route } from 'react-router-dom@6'
 import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
 import Routes from './routes/routes.js'
 import Menu from './Pages/Menu.jsx'
 import Home from './Pages/Home.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <BrowserRouter>
   <React.StrictMode>
     <App/>
   </React.StrictMode>
- </BrowserRouter>
+  </BrowserRouter>
+
 
 );
 
